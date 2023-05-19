cbdecf7727dbfb268777db6ddb692a1329eb754c
@brandon.monsalve <brandon.monsalve@alumnos.inf.utfsm.cl>
2023-05-18 20:45:56 -0400

    Delete

--- BACKEND/list/api.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, permissions
-from list.models import Item
-from list.serializers import ListSerializer
-
-
-class ListViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = ListSerializer
--- BACKEND/list/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import ListViewSet
-
-router = routers.DefaultRouter()
-
-router.register('api/list', ListViewSet, 'listas')
-
-urlpatterns = router.urls
